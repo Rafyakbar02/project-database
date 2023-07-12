eb99c78f09f9f10f5616f0985c045d9d5a3d44b9
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-12 18:54:17 +0700

    changed naming, extension, and added TopBar

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
-import Navbar from "@/components/navbar"
+import Navbar from "@/components/Navbar";
+import BottomNav from "@/components/BottomNav";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Navbar />
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <Navbar />
+        <BottomNav />
       </div>
     </main>
-  )
+  );
 }
