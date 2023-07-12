3183bac93cfd450f9b91e96aab76dc7f39ab6af0
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-12 20:17:05 +0700

    linked nav buttons to pages

--- components/BottomNav.jsx
+++ components/BottomNav.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import Link from "next/link";
 
 function BottomNav() {
   return (
     <div class="fixed bottom-0 left-0 z-50 w-full h-20 bg-red-700">
       <div class="flex h-full w-full mx-auto font-medium justify-around">
-        <button type="button" class="px-5 hover:bg-red-600 group h-full w-full">
+        <Link
+          href="/pupr"
+          class="px-5 hover:bg-red-600 flex w-full items-center justify-center"
+        >
           <span class="text-xl sm:text-2xl text-white">PUPR</span>
-        </button>
-        <button type="button" class="px-5 hover:bg-red-600 group h-full w-full">
+        </Link>
+        <Link
+          href="non-pupr"
+          class="px-5 hover:bg-red-600 flex w-full items-center justify-center"
+        >
           <span class="text-xl sm:text-2xl text-white">Non-PUPR</span>
-        </button>
-        <button type="button" class="px-5 hover:bg-red-600 group h-full w-full">
+        </Link>
+        <Link
+          href="pdf"
+          class="px-5 hover:bg-red-600 flex w-full items-center justify-center"
+        >
           <span class="text-xl sm:text-2xl text-white">PDF</span>
-        </button>
+        </Link>
       </div>
     </div>
   );
