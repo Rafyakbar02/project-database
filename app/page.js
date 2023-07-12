97b02050a780c824ddf551406f41bcb2de9d278f
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-12 19:41:10 +0700

    moved Navbar and BottomNav components to layout.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,3 @@
-import Navbar from "@/components/Navbar";
-import BottomNav from "@/components/BottomNav";
-
 export default function Home() {
-  return (
-    <main className="">
-      <Navbar />
-      <BottomNav />
-    </main>
-  );
+  return <main className=""></main>;
 }
