e3dc0c5312544248b7f86a1a20f08dabed3059e7
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-12 18:54:47 +0700

    changed naming, extension, added Navbar

---
+++ components/BottomNav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function BottomNav() {
+  return (
+    <div class="fixed bottom-0 left-0 z-50 w-full h-20 bg-red-700">
+      <div class="flex h-full w-full mx-auto font-medium justify-around">
+        <button type="button" class="px-5 hover:bg-red-600 group h-full w-full">
+          <span class="text-2xl text-stone-300">PUPR</span>
+        </button>
+        <button type="button" class="px-5 hover:bg-red-600 group h-full w-full">
+          <span class="text-2xl text-stone-300">Non-PUPR</span>
+        </button>
+        <button type="button" class="px-5 hover:bg-red-600 group h-full w-full">
+          <span class="text-2xl text-stone-300">PDF</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default BottomNav;
---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="h-16 bg-white w-full">
+      <h1 className="text-sm text-black">Test</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
