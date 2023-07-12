6ac54eb97befe40188cd6dc78978da750783527a
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-12 19:01:24 +0700

    removed redundant classes

--- app/page.js
+++ app/page.js
@@ -3,11 +3,9 @@ import BottomNav from "@/components/BottomNav";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <Navbar />
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <BottomNav />
-      </div>
+      <BottomNav />
     </main>
   );
 }
