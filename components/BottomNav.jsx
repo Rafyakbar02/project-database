ce2ea060327911b69f8f561ed9a659b905199c86
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-12 19:20:37 +0700

    tweaked font size for mobile

--- components/BottomNav.jsx
+++ components/BottomNav.jsx
@@ -5,13 +5,13 @@ function BottomNav() {
     <div class="fixed bottom-0 left-0 z-50 w-full h-20 bg-red-700">
       <div class="flex h-full w-full mx-auto font-medium justify-around">
         <button type="button" class="px-5 hover:bg-red-600 group h-full w-full">
-          <span class="text-2xl text-stone-300">PUPR</span>
+          <span class="text-xl sm:text-2xl text-white">PUPR</span>
         </button>
         <button type="button" class="px-5 hover:bg-red-600 group h-full w-full">
-          <span class="text-2xl text-stone-300">Non-PUPR</span>
+          <span class="text-xl sm:text-2xl text-white">Non-PUPR</span>
         </button>
         <button type="button" class="px-5 hover:bg-red-600 group h-full w-full">
-          <span class="text-2xl text-stone-300">PDF</span>
+          <span class="text-xl sm:text-2xl text-white">PDF</span>
         </button>
       </div>
     </div>
