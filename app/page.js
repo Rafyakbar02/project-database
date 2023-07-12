70481e7794f475e4b70c0cb24402df5bc37f60ee
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-12 20:06:54 +0700

    added small info cards

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+import InfoCard from "@/components/InfoCard";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <section className="p-4 flex flex-row gap-4">
+      <InfoCard number={141} type={"Total Projects"} />
+      <InfoCard number={33} type={"Signed Projects"} />
+      <InfoCard number={108} type={"Projects In-progress"} />
+      <InfoCard number={"344.44T"} type={"Total Investment Amount (Rp.)"} />
+      <InfoCard number={"77.25T"} type={"Total Exposure Amount (Rp.)"} />
+    </section>
+  );
 }
---
+++ components/InfoCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InfoCard = ({ number, type }) => {
+  return (
+    <div className="bg-gray-100 rounded-2xl flex flex-col items-start justify-center gap-1 p-4">
+      <h2 className="text-4xl font-bold">{number}</h2>
+      <h3 className="text-sm">{type}</h3>
+    </div>
+  );
+};
+
+export default InfoCard;
