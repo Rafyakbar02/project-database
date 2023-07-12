ca7544e556d7620110e1e5c4076420ab2b783a59
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-12 20:56:57 +0700

    added carousel effect

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import InfoCard from "@/components/InfoCard";
 
 export default function Home() {
   return (
-    <section className="p-4 flex flex-row gap-4">
+    <section className="p-4 flex flex-row gap-4 overflow-x-auto">
       <InfoCard number={141} type={"Total Projects"} />
       <InfoCard number={33} type={"Signed Projects"} />
       <InfoCard number={108} type={"Projects In-progress"} />
--- components/InfoCard.jsx
+++ components/InfoCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const InfoCard = ({ number, type }) => {
   return (
-    <div className="bg-gray-100 rounded-2xl flex flex-col items-start justify-center gap-1 p-4">
+    <div className="bg-gray-100 rounded-2xl flex flex-col items-start justify-center gap-1 p-4 h-24 min-w-fit">
       <h2 className="text-4xl font-bold">{number}</h2>
       <h3 className="text-sm">{type}</h3>
     </div>
