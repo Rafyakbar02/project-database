17304beef1caba02b256749f08f2d81bc4e452b5
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-18 16:20:36 +0700

    added constants for temp info

---
+++ constants/index.js
@@ -0,0 +1,67 @@
+export const produk = [
+  {
+    id: "produk-1",
+    title: "KPBU",
+  },
+  {
+    id: "produk-2",
+    title: "Non-KPBU",
+  },
+  {
+    id: "produk-3",
+    title: "PDF",
+  },
+];
+
+export const sektor = [
+  {
+    id: "sektor-1",
+    title: "Jalan",
+  },
+  {
+    id: "sektor-2",
+    title: "Pariwisata",
+  },
+  {
+    id: "sektor-3",
+    title: "Telekomunikasi",
+  },
+  {
+    id: "sektor-4",
+    title: "Ketenagalistrikan",
+  },
+  {
+    id: "sektor-5",
+    title: "Transportasi",
+  },
+];
+
+export const tahapan = [
+  {
+    id: "tahapan-1",
+    title: "Planning",
+  },
+  {
+    id: "tahapan-2",
+    title: "Signing",
+  },
+  {
+    id: "tahapan-3",
+    title: "Finished",
+  },
+];
+
+export const pemilik = [
+  {
+    id: "pemilik-1",
+    title: "Kementerian PUPR",
+  },
+  {
+    id: "pemilik-2",
+    title: "Kementerian Pendidikan",
+  },
+  {
+    id: "pemilik-3",
+    title: "Kementerian Keuangan",
+  },
+];
