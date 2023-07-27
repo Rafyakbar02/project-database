39971e29a6c9a5404770e1600685771c6b72990f
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-27 17:37:04 +0700

    added filters panel to details page

--- app/details/page.jsx
+++ app/details/page.jsx
@@ -1,23 +1,27 @@
 import Accordion from "@/components/Accordion";
 import React from "react";
 import { projects } from "@/constants";
+import FiltersPanel from "@/components/FiltersPanel";
 
 function Details() {
-    return (
-        <div>
-            {projects.map((p, i) => (
-                <Accordion
-                    i={i + 1}
-                    title={p.title}
-                    division={p.division}
-                    totalInvestment={p.totalInvestment}
-                    signingDate={p.signingDate}
-                    totalExposure={p.totalExposure}
-                    effectiveDate={p.effectiveDate}
-                />
-            ))}
-        </div>
-    );
+  return (
+    <div>
+      <FiltersPanel />
+      <div className="flex flex-col gap-4">
+        {projects.map((p, i) => (
+          <Accordion
+            i={i + 1}
+            title={p.title}
+            division={p.division}
+            totalInvestment={p.totalInvestment}
+            signingDate={p.signingDate}
+            totalExposure={p.totalExposure}
+            effectiveDate={p.effectiveDate}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Details;
