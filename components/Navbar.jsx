ba3e82680e9c677f12a13614094736c2780c3428
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-27 17:34:54 +0700

    edited margin for tablet and above size

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="bg-white w-full p-4 flex flex-row justify-between items-center shadow">
-      <Link href="/">
-        <h1 className="text-black text-xl font-bold">Executive Dashboard</h1>
-      </Link>
-      <Profile />
+    <nav className="bg-white w-full px-4 sm:px-0 py-4 shadow">
+      <div className="flex flex-row justify-between items-center md:mx-20">
+        <Link href="/">
+          <h1 className="text-black text-xl font-bold">Executive Dashboard</h1>
+        </Link>
+        <Profile />
+      </div>
     </nav>
   );
 };
