4b9ed561fa87c9018a54ca32f46001b2b462fe24
Rafy Akbar <rafyakbar02@gmail.com>
2023-07-27 18:11:23 +0700

    added new projects

--- constants/index.js
+++ constants/index.js
@@ -1,86 +1,102 @@
 export const produk = [
-    {
-        id: "produk-1",
-        title: "KPBU",
-    },
-    {
-        id: "produk-2",
-        title: "Non-KPBU",
-    },
-    {
-        id: "produk-3",
-        title: "PDF",
-    },
+  {
+    id: "produk-1",
+    title: "KPBU",
+  },
+  {
+    id: "produk-2",
+    title: "Non-KPBU",
+  },
+  {
+    id: "produk-3",
+    title: "PDF",
+  },
 ];
 
 export const sektor = [
-    {
-        id: "sektor-1",
-        title: "Jalan",
-    },
-    {
-        id: "sektor-2",
-        title: "Pariwisata",
-    },
-    {
-        id: "sektor-3",
-        title: "Telekomunikasi",
-    },
-    {
-        id: "sektor-4",
-        title: "Ketenagalistrikan",
-    },
-    {
-        id: "sektor-5",
-        title: "Transportasi",
-    },
+  {
+    id: "sektor-1",
+    title: "Jalan",
+  },
+  {
+    id: "sektor-2",
+    title: "Pariwisata",
+  },
+  {
+    id: "sektor-3",
+    title: "Telekomunikasi",
+  },
+  {
+    id: "sektor-4",
+    title: "Ketenagalistrikan",
+  },
+  {
+    id: "sektor-5",
+    title: "Transportasi",
+  },
 ];
 
 export const tahapan = [
-    {
-        id: "tahapan-1",
-        title: "Planning",
-    },
-    {
-        id: "tahapan-2",
-        title: "Signing",
-    },
-    {
-        id: "tahapan-3",
-        title: "Finished",
-    },
+  {
+    id: "tahapan-1",
+    title: "Planning",
+  },
+  {
+    id: "tahapan-2",
+    title: "Signing",
+  },
+  {
+    id: "tahapan-3",
+    title: "Finished",
+  },
 ];
 
 export const pemilik = [
-    {
-        id: "pemilik-1",
-        title: "Kementerian PUPR",
-    },
-    {
-        id: "pemilik-2",
-        title: "Kementerian Pendidikan",
-    },
-    {
-        id: "pemilik-3",
-        title: "Kementerian Keuangan",
-    },
+  {
+    id: "pemilik-1",
+    title: "Kementerian PUPR",
+  },
+  {
+    id: "pemilik-2",
+    title: "Kementerian Pendidikan",
+  },
+  {
+    id: "pemilik-3",
+    title: "Kementerian Keuangan",
+  },
 ];
 
 export const projects = [
-    {
-        title: "Patimban Access Toll Road",
-        division: "Minister For Public Works and Human Settlements",
-        totalInvestment: "5,02 Trillion (IDR)",
-        signingDate: "24 Jan 2023",
-        totalExposure: "1,10 Trillion (IDR)",
-        effectiveDate: "1 Feb 2023",
-    },
-    {
-        title: "Madiun Street Lighting Equipment Project",
-        division: "Madiun Regency Government",
-        totalInvestment: "100 Billion (IDR)",
-        signingDate: "20 Sep 2022",
-        totalExposure: "10 Billion (IDR)",
-        effectiveDate: "1 Oct 2022",
-    },
+  {
+    title: "Patimban Access Toll Road",
+    division: "Minister For Public Works and Human Settlements",
+    totalInvestment: "5,02 Trillion (IDR)",
+    signingDate: "24 Jan 2023",
+    totalExposure: "1,10 Trillion (IDR)",
+    effectiveDate: "1 Feb 2023",
+  },
+  {
+    title: "Madiun Street Lighting Equipment Project",
+    division: "Madiun Regency Government",
+    totalInvestment: "100 Billion (IDR)",
+    signingDate: "20 Sep 2022",
+    totalExposure: "10 Billion (IDR)",
+    effectiveDate: "1 Oct 2022",
+  },
+  {
+    title: "Palapa Ring Project - Western Package",
+    division: "Ministry of Telecommunication and Informatics",
+    totalInvestment: "1,2 Trillion (IDR)",
+    signingDate: "29 Feb 2016",
+    totalExposure: "14,9 Trillion (IDR)",
+    effectiveDate: "10 Nov 2016",
+  },
+  {
+    title: "Central Java Power Plant Project",
+    division: "PT Perusahaan Listrik Negara (PLN)",
+    totalInvestment: "61,4 Trillion (IDR)",
+    signingDate: "06 Oct 2011",
+    totalExposure: "-",
+    effectiveDate: "03 Jun 2016",
+  },
 ];
