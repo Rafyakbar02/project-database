a25ea49bb110b0984ce3c11e1782b57d6f66dc11
Samuel Kosasih <samadrianko@gmail.com>
2023-08-09 13:50:13 -0400

    Added title to modal

--- app/details/page.jsx
+++ app/details/page.jsx
@@ -10,119 +10,123 @@ import { projects, product, sector, phase, pjkp, priority } from "@/constants";
 const categories = [product, sector, priority, phase, pjkp];
 
 function Details() {
-  const [query, setQuery] = useState("");
-  const [checkList, setCheckList] = useState(categories);
-  const [showModal, setShowModal] = useState(false);
-  const [filteredProjects, setFilteredProjects] = useState(projects);
-  const [numOfFilters, setNumOfFilters] = useState(0);
+    const [query, setQuery] = useState("");
+    const [checkList, setCheckList] = useState(categories);
+    const [showModal, setShowModal] = useState(false);
+    const [filteredProjects, setFilteredProjects] = useState(projects);
+    const [numOfFilters, setNumOfFilters] = useState(0);
 
-  const handleQuery = (e) => {
-    setQuery(e.target.value);
-  };
+    const handleQuery = (e) => {
+        setQuery(e.target.value);
+    };
 
-  const handleCheck = (event, categoryIndex, subCategoryIndex) => {
-    checkList[categoryIndex][subCategoryIndex].checked = event.target.checked;
-    setCheckList([...checkList]);
-  };
+    const handleCheck = (event, categoryIndex, subCategoryIndex) => {
+        checkList[categoryIndex][subCategoryIndex].checked =
+            event.target.checked;
+        setCheckList([...checkList]);
+    };
 
-  const resetCheckList = () => {
-    for (var category of checkList) {
-      for (var subCategory of category) {
-        subCategory.checked = false;
-      }
-    }
-    setCheckList([...checkList]);
-  };
-
-  const handleClear = () => {
-    setQuery("");
-  };
-
-  const handleShowModal = () => {
-    setShowModal(true);
-  };
-
-  const showResult = (value) => {
-    const result = filteredProjects.filter(
-      (p) =>
-        p.title.toLowerCase().includes(value.toLowerCase()) ||
-        p.division.toLowerCase().includes(value.toLowerCase())
-    );
-    return result;
-  };
-
-  const submitFilter = () => {
-    var selected = groupSelected();
-    if (selected.length > 0) {
-      const result = projects.filter(
-        (p) =>
-          selected.includes(p.product) ||
-          selected.includes(p.sector) ||
-          selected.includes(p.phase) ||
-          selected.includes(p.priority) ||
-          selected.includes(p.division)
-      );
-      setNumOfFilters(selected.length);
-      setFilteredProjects(result);
-    } else {
-      setNumOfFilters(0);
-      setFilteredProjects(projects);
-    }
-  };
-
-  const groupSelected = () => {
-    var selected = [];
-    for (var category of checkList) {
-      for (var subCategory of category) {
-        if (subCategory.checked) {
-          selected.push(subCategory.label);
+    const resetCheckList = () => {
+        for (var category of checkList) {
+            for (var subCategory of category) {
+                subCategory.checked = false;
+            }
         }
-      }
-    }
-    return selected;
-  };
+        setCheckList([...checkList]);
+    };
 
-  return (
-    <div>
-      <div className="flex flex-col gap-4 my-4">
-        <Panel
-          query={query}
-          handleClear={handleClear}
-          handleQuery={handleQuery}
-          handleShowModal={handleShowModal}
-          numOfFilters={numOfFilters}
-        />
-        {showResult(query).length > 0 ? (
-          <h5 className="mb-0 md:mx-20 mx-4">{`Showing ${
-            showResult(query).length
-          } projects`}</h5>
-        ) : null}
-        {showResult(query)
-          .sort((a, b) => (a.title > b.title ? 1 : -1))
-          .map((p, i) => (
-            <Accordion
-              i={i + 1}
-              key={p.id}
-              title={p.title}
-              division={p.division}
-              totalInvestment={p.totalInvestment}
-              signingDate={p.signingDate}
-              totalExposure={p.totalExposure}
-              effectiveDate={p.effectiveDate}
+    const handleClear = () => {
+        setQuery("");
+    };
+
+    const handleShowModal = () => {
+        setShowModal(true);
+    };
+
+    const showResult = (value) => {
+        const result = filteredProjects.filter(
+            (p) =>
+                p.title.toLowerCase().includes(value.toLowerCase()) ||
+                p.division.toLowerCase().includes(value.toLowerCase())
+        );
+        return result;
+    };
+
+    const submitFilter = () => {
+        var selected = groupSelected();
+        if (selected.length > 0) {
+            const result = projects.filter(
+                (p) =>
+                    selected.includes(p.product) ||
+                    selected.includes(p.sector) ||
+                    selected.includes(p.phase) ||
+                    selected.includes(p.priority) ||
+                    selected.includes(p.division)
+            );
+            setNumOfFilters(selected.length);
+            setFilteredProjects(result);
+        } else {
+            setNumOfFilters(0);
+            setFilteredProjects(projects);
+        }
+    };
+
+    const groupSelected = () => {
+        var selected = [];
+        for (var category of checkList) {
+            for (var subCategory of category) {
+                if (subCategory.checked) {
+                    selected.push(subCategory.label);
+                }
+            }
+        }
+        return selected;
+    };
+
+    return (
+        <div>
+            <div className="flex flex-col gap-4 my-4">
+                <Panel
+                    query={query}
+                    handleClear={handleClear}
+                    handleQuery={handleQuery}
+                    handleShowModal={handleShowModal}
+                    numOfFilters={numOfFilters}
+                />
+                {showResult(query).length > 0 ? (
+                    <h5 className="mb-0 md:mx-20 mx-4">{`Showing ${
+                        showResult(query).length
+                    } projects`}</h5>
+                ) : null}
+                {showResult(query)
+                    .sort((a, b) => (a.title > b.title ? 1 : -1))
+                    .map((p, i) => (
+                        <Accordion
+                            i={i + 1}
+                            key={p.id}
+                            title={p.title}
+                            division={p.division}
+                            totalInvestment={p.totalInvestment}
+                            signingDate={p.signingDate}
+                            totalExposure={p.totalExposure}
+                            effectiveDate={p.effectiveDate}
+                        />
+                    ))}
+                {showResult(query).length == 0 ? (
+                    <NotFound query={query} />
+                ) : null}
+            </div>
+            <Modal
+                title="Filters"
+                checkList={checkList}
+                showModal={showModal}
+                setShowModal={setShowModal}
+                handleCheck={handleCheck}
+                resetCheckList={resetCheckList}
+                submitFilter={submitFilter}
             />
-          ))}
-        {showResult(query).length == 0 ? <NotFound query={query} /> : null}
-      </div>
-      <Modal
-        checkList={checkList}
-        showModal={showModal}
-        setShowModal={setShowModal}
-        handleCheck={handleCheck}
-        resetCheckList={resetCheckList}
-        submitFilter={submitFilter}
-      />
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Details;
